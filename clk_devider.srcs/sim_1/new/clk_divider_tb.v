c3a670b79ab1e3d3c191b67c7633cfbae8196e0d
jan pękalski <janpekalski939@gmail.com>
2026-08-13 21:44:19 +0200

    init

---
+++ clk_devider.srcs/sim_1/new/clk_divider_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.08.2026 21:43:21
+// Design Name: 
+// Module Name: clk_divider_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clk_divider_tb(
+
+    );
+endmodule
---
+++ clk_devider.srcs/sources_1/new/clk_divider.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.08.2026 21:43:04
+// Design Name: 
+// Module Name: clk_divider
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clk_divider(
+
+    );
+endmodule
